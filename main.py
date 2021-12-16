9ab73e3972ca39a6b9d7c84f7b7bc2e2423adb38
arha83 <amirrhosseinali@gmail.com>
2021-12-16 21:33:44 +0330

    loading and mapping database

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import tensorflow as tf
+from tensorflow import keras
 from tensorflow.keras import datasets, layers, models
 
 
-classes= ['Airplane','Automobile','Bird','Cat','Deer','Dog','Frog','Horse','Ship','Truck']
+# data classes:
+classes= ['airplane','automobile','bird','cat','deer','dog','frog','horse','ship','truck']
+
+# loading dataset:
+(trainImages, trainLabels), (testImages, testLables)= datasets.cifar10.load_data()
+# normalizing dataset:
+trainImages, testImages= trainImages/255.0, testImages/255.0
+
