1c358ff5cd5da25049b162b5c245a2aa8abd106a
arha83 <amirrhosseinali@gmail.com>
2021-12-17 16:28:27 +0330

    added ui

--- main.py
+++ main.py
@@ -25,43 +25,53 @@ truck : 9
 classes= ['airplane','automobile','bird','cat','deer','dog','frog','horse','ship','truck']
 
 # loading dataset:
-'''
 print('### loading dataset...')
 (trainImages, trainLabels), (testImages, testLables)= datasets.cifar10.load_data()
 # normalizing dataset:
 print('### resizing dataset...')
 trainImages, testImages= trainImages/255.0, testImages/255.0
 
-# desgining tme model:
-print('### building model...')
-model= models.Sequential()
-model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)))
-model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)))
-model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)))
-model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10))
-# loading model:
-print('### loading model...')
-model= models.load_model(os.path.dirname(os.path.abspath(__file__))+'\\myModel')
-'''
-# training:
-'''
-print('### training...')
-model.compile(optimizer='adam', loss=losses.SparseCategoricalCrossentropy(from_logits=True),metrics=['accuracy'])
-model.fit(trainImages, trainLabels, epochs=10, validation_data=(testImages, testLables))
-model.save(os.path.dirname(os.path.abspath(__file__))+'\\myModel')
-'''
+ch1= input('### do you want new model? (y/n) ')
+if ch1 == 'y':
+    # desgining the model:
+    print('### building model...')
+    model= models.Sequential()
+    model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)))
+    model.add(layers.MaxPooling2D((2,2)))
+    model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)))
+    model.add(layers.MaxPooling2D((2,2)))
+    model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)))
+    model.add(layers.MaxPooling2D((2,2)))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(10))
 
-model= models.load_model(os.path.dirname(os.path.abspath(__file__))+'\\myModel')
-image= cv.imread('C:\\Users\\amirr\\Desktop\\cifar10\\ship.jpg')
-image= cv.cvtColor(image, cv.COLOR_BGR2RGB)
-pre= np.expand_dims(image, 0)
-predictions= model.predict([pre])
-maxi= np.where(predictions[0] == np.amax(predictions[0]))[0][0]
-for i in range(10):
-    print(classes[i], ': ', predictions[0][i], sep='')
-print('\nclosest class:',classes[maxi],'\n\n')
+else:
+    # loading model:
+    print('### loading model...')
+    model= models.load_model(os.path.dirname(os.path.abspath(__file__))+'\\myModel')
+print(model.summary())
+
+# training:
+ch2= 'y'
+if ch1 != 'y': ch2= input('### do you want to train model? (y/n) ')
+if ch2 == 'y' or ch1 == 'y':
+    epochs= int(input('enter the number of epochs for training: '))
+    print('### training...')
+    model.compile(optimizer='adam', loss=losses.SparseCategoricalCrossentropy(from_logits=True),metrics=['accuracy'])
+    model.fit(trainImages, trainLabels, epochs=epochs, validation_data=(testImages, testLables))
+    model.save(os.path.dirname(os.path.abspath(__file__))+'\\myModel')
+
+# predicting:
+while True:
+    path= input('enter the image path (enter q to exit)\n')
+    if path == 'q': break
+    image= cv.imread(path)
+    image= cv.cvtColor(image, cv.COLOR_BGR2RGB)
+    pre= np.expand_dims(image, 0)
+    predictions= model.predict([pre])
+    maxi= np.where(predictions[0] == np.amax(predictions[0]))[0][0]
+    for i in range(10): print(classes[i], ': ', predictions[0][i], sep='')
+    print('\nclosest class:',classes[maxi],'\n\n')
+
+print('\n\n')
